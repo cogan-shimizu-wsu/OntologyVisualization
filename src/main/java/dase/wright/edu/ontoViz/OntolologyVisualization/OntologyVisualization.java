f8aceb28db9bcf3592a17f034c4b95887fa2dbdb
nazifakarima <nazifakarima@gmail.com>
2017-03-01 15:28:14 -0500

    init graph

--- src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/OntologyVisualization.java
+++ src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/OntologyVisualization.java
@@ -19,7 +19,14 @@ import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
+import com.yworks.yfiles.geometry.RectD;
+import com.yworks.yfiles.graph.IEdge;
+import com.yworks.yfiles.graph.IGraph;
+import com.yworks.yfiles.graph.ILabel;
+import com.yworks.yfiles.graph.INode;
+import com.yworks.yfiles.graph.IPort;
 import com.yworks.yfiles.view.GraphComponent;
+import com.yworks.yfiles.view.input.GraphEditorInputMode;
 
 /**
  * Hello world!
@@ -28,16 +35,7 @@ import com.yworks.yfiles.view.GraphComponent;
 public class OntologyVisualization {
 
 	public OntologyVisualization() {
-		// create a component for displaying and editing a graph.
-		GraphComponent graphComponent = new GraphComponent();
-		// create a top-level window and add the graph component.
-		JFrame frame = new JFrame("Hello, yFiles for Java");
 
-		frame.setSize(500, 500);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.add(graphComponent, BorderLayout.CENTER);
-		frame.setVisible(true);
 	}
 
 	public static OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
@@ -57,15 +55,37 @@ public class OntologyVisualization {
 	public static void main(String[] args) {
 
 		Runnable doHelloWorld = new Runnable() {
-		     public void run() {
-		         System.out.println("Hello World on " + Thread.currentThread());
-		     }
-		 }; 
+			public void run() {
+				// create a component for displaying and editing a graph.
+				GraphComponent graphComponent = new GraphComponent();
+				// Enabling user-interaction
+				graphComponent.setInputMode(new GraphEditorInputMode());
+
+				IGraph graph = graphComponent.getGraph();
+				INode node1 = graph.createNode(new RectD(30, 30, 30, 30));
+				INode node2 = graph.createNode(new RectD(100, 0, 30, 30));
+
+				IEdge edge1 = graph.createEdge(node1, node2);
+
+				IPort portAtNode1 = graph.addPort(node1);
+
+				ILabel ln1 = graph.addLabel(node1, "n1");
+				ILabel ln2 = graph.addLabel(node2, "n2");
+
+				// create a top-level window and add the graph component.
+				JFrame frame = new JFrame("Ontology Visualization");
+
+				frame.setSize(500, 500);
+				frame.setLocationRelativeTo(null);
+				frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+				frame.add(graphComponent, BorderLayout.CENTER);
+				frame.setVisible(true);
+			}
+		};
 		SwingUtilities.invokeLater(doHelloWorld);
 
 		init(manager, fullOntology, classes, prop, dataProp);
 	}
-
 
 	private static void init(OWLOntologyManager manager, File fullOntology, ArrayList<OWLClass> classes,
 			ArrayList<OWLObjectProperty> prop, ArrayList<OWLDataProperty> dataProp) {
@@ -74,23 +94,26 @@ public class OntologyVisualization {
 			ontology = manager.loadOntologyFromOntologyDocument(fullOntology);
 			for (OWLClass cls : ontology.getClassesInSignature()) {
 				classes.add(cls);
-				//System.out.println("Class: " + classes.get(classes.size() - 1));
+				// System.out.println("Class: " + classes.get(classes.size() -
+				// 1));
 				for (OWLObjectPropertyDomainAxiom op : ontology.getAxioms(AxiomType.OBJECT_PROPERTY_DOMAIN)) {
 					if (op.getDomain().equals(cls)) {
 						objPropAxioms.put(cls, (OWLObjectProperty) op.getProperty());
 					}
 				}
 			}
-			//writeHashMapintoFile(HashMap<K, V>, File path);
+			// writeHashMapintoFile(HashMap<K, V>, File path);
 			for (OWLObjectProperty obp : ontology.getObjectPropertiesInSignature()) {
 				prop.add(obp);
-				//System.out.println("Object Property:" + prop.get(prop.size() - 1));
+				// System.out.println("Object Property:" + prop.get(prop.size()
+				// - 1));
 			}
 			for (OWLDataProperty dp : ontology.getDataPropertiesInSignature()) {
 				dataProp.add(dp);
-				//System.out.println("Data Properties " + dataProp.get(dataProp.size() - 1));
+				// System.out.println("Data Properties " +
+				// dataProp.get(dataProp.size() - 1));
 			}
-			
+
 			showAllAxioms();
 		} catch (OWLOntologyCreationException e) {
 			// TODO Auto-generated catch block
@@ -98,10 +121,9 @@ public class OntologyVisualization {
 		}
 	}
 
-
 	private static void showAllAxioms() {
-		for(OWLClass cls: classes){
-			
+		for (OWLClass cls : classes) {
+
 		}
 	}
 }
