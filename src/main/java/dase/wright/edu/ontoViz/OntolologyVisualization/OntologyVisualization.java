09251586993df7576f7cfa6a45ca6ef5505d8959
nazifakarima <nazifakarima@gmail.com>
2017-03-07 08:20:23 -0500

    Not Much Progress

--- src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/OntologyVisualization.java
+++ src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/OntologyVisualization.java
@@ -1,16 +1,23 @@
 package dase.wright.edu.ontoViz.OntolologyVisualization;
 
 import java.awt.BorderLayout;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
@@ -19,14 +26,11 @@ import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
-import com.yworks.yfiles.geometry.RectD;
-import com.yworks.yfiles.graph.IEdge;
 import com.yworks.yfiles.graph.IGraph;
-import com.yworks.yfiles.graph.ILabel;
-import com.yworks.yfiles.graph.INode;
-import com.yworks.yfiles.graph.IPort;
 import com.yworks.yfiles.view.GraphComponent;
 import com.yworks.yfiles.view.input.GraphEditorInputMode;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLAxiomImpl;
 
 /**
  * Hello world!
@@ -45,7 +49,7 @@ public class OntologyVisualization {
 	public static ArrayList<OWLClass> classes = new ArrayList<OWLClass>();
 	public static ArrayList<OWLObjectProperty> prop = new ArrayList<OWLObjectProperty>();
 	public static ArrayList<OWLDataProperty> dataProp = new ArrayList<OWLDataProperty>();
-	public static HashMap<OWLClass, OWLObjectProperty> objPropAxioms = new HashMap<>();
+	public static HashMap<OWLClass, ArrayList<OWLObjectProperty>> objPropAxioms = new HashMap<OWLClass, ArrayList<OWLObjectProperty>>();
 	// prop = (ArrayList<OWLObjectProperty>)
 	// ontology.getObjectPropertiesInSignature();
 	// dataProp = (ArrayList<OWLDataProperty>)
@@ -54,7 +58,8 @@ public class OntologyVisualization {
 
 	public static void main(String[] args) {
 
-		Runnable doHelloWorld = new Runnable() {
+init(manager, fullOntology, classes, prop, dataProp);
+/*		Runnable doHelloWorld = new Runnable() {
 			public void run() {
 				// create a component for displaying and editing a graph.
 				GraphComponent graphComponent = new GraphComponent();
@@ -63,7 +68,7 @@ public class OntologyVisualization {
 
 				IGraph graph = graphComponent.getGraph();
 				INode node1 = graph.createNode(new RectD(30, 30, 30, 30));
-				INode node2 = graph.createNode(new RectD(100, 0, 30, 30));
+				INode node2 = graph.createNode(new RectD(100, 30, 30, 30));
 
 				IEdge edge1 = graph.createEdge(node1, node2);
 
@@ -82,9 +87,7 @@ public class OntologyVisualization {
 				frame.setVisible(true);
 			}
 		};
-		SwingUtilities.invokeLater(doHelloWorld);
-
-		init(manager, fullOntology, classes, prop, dataProp);
+		SwingUtilities.invokeLater(doHelloWorld);*/
 	}
 
 	private static void init(OWLOntologyManager manager, File fullOntology, ArrayList<OWLClass> classes,
@@ -94,15 +97,24 @@ public class OntologyVisualization {
 			ontology = manager.loadOntologyFromOntologyDocument(fullOntology);
 			for (OWLClass cls : ontology.getClassesInSignature()) {
 				classes.add(cls);
-				// System.out.println("Class: " + classes.get(classes.size() -
-				// 1));
-				for (OWLObjectPropertyDomainAxiom op : ontology.getAxioms(AxiomType.OBJECT_PROPERTY_DOMAIN)) {
-					if (op.getDomain().equals(cls)) {
-						objPropAxioms.put(cls, (OWLObjectProperty) op.getProperty());
-					}
+			//	 System.out.println("Class: " + classes.get(classes.size() - 1));
+				for (OWLAxiom op : ontology.getAxioms()) {
+					System.out.println("Axiom:" + op);
+					/*if (op.getDomain().equals(cls)) {
+						System.out.println("Domain Class" + cls);
+						if(objPropAxioms.containsKey(cls)){
+							ArrayList<OWLObjectProperty> properties = objPropAxioms.get(cls);
+							properties.add((OWLObjectProperty) op.getProperty());
+							objPropAxioms.put(cls, properties);
+						}else {
+							ArrayList<OWLObjectProperty> properties = new ArrayList<OWLObjectProperty>();
+							properties.add((OWLObjectProperty) op.getProperty());
+							objPropAxioms.put(cls, properties);
+						}
+					}*/
 				}
 			}
-			// writeHashMapintoFile(HashMap<K, V>, File path);
+			writeHashMapintoFile(objPropAxioms, "ChessGame");
 			for (OWLObjectProperty obp : ontology.getObjectPropertiesInSignature()) {
 				prop.add(obp);
 				// System.out.println("Object Property:" + prop.get(prop.size()
@@ -114,16 +126,41 @@ public class OntologyVisualization {
 				// dataProp.get(dataProp.size() - 1));
 			}
 
-			showAllAxioms();
+			showAllAxioms(objPropAxioms);
 		} catch (OWLOntologyCreationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	private static void showAllAxioms() {
-		for (OWLClass cls : classes) {
-
+	private static void writeHashMapintoFile(HashMap<OWLClass, ArrayList<OWLObjectProperty>> objPropAxioms,
+			String string) {
+//		File file = new File("/Users/nazifakarima/Desktop", "chessgame");
+//		
+//		//Blank workbook
+//        XSSFWorkbook workbook = new XSSFWorkbook(); 
+//         
+//        //Create a blank sheet
+//        XSSFSheet sheet = workbook.createSheet("ChessGame");
+//        
+//        Iterator it = objPropAxioms.entrySet().iterator();
+//        while (it.hasNext()) {
+//            Map.Entry pair = (Map.Entry)it.next();
+//            System.out.println(pair.getKey() + " = " + pair.getValue());
+//        }
+		BufferedWriter writer = null;
+		File chessGame = new File("chessGame");
+		try {
+			System.out.println(chessGame.getCanonicalPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+
+	private static void showAllAxioms(HashMap<OWLClass, ArrayList<OWLObjectProperty>> objPropAxioms2) {
+		//for (Iterator it:objPropAxioms2) {
+
+		//}
 	}
 }
