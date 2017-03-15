f35a72b0de5f0a0fa4dec057632b2f39f7848509
nazifakarima <nazifakarima@gmail.com>
2017-03-15 17:29:04 -0400

    yWorks displays properly having license file placed at src/main/java

--- src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/OntologyVisualization.java
+++ src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/OntologyVisualization.java
@@ -1,10 +1,15 @@
 package dase.wright.edu.ontoViz.OntolologyVisualization;
 
+import java.awt.BorderLayout;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.MissingImportHandlingStrategy;
@@ -15,6 +20,15 @@ import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import com.yworks.yfiles.geometry.RectD;
+import com.yworks.yfiles.graph.IEdge;
+import com.yworks.yfiles.graph.IGraph;
+import com.yworks.yfiles.graph.ILabel;
+import com.yworks.yfiles.graph.INode;
+import com.yworks.yfiles.graph.IPort;
+import com.yworks.yfiles.view.GraphComponent;
+import com.yworks.yfiles.view.input.GraphEditorInputMode;
 
 /**
  * This Project is supposed to produce an interactive visualization for ontologies for ensuring better understandability
@@ -38,34 +52,32 @@ public class OntologyVisualization {
 
 	public static void main(String[] args) {
 
-		System.out.println("Filepath: " + fullOntology.getAbsolutePath());
+		//System.out.println("Filepath: " + fullOntology.getAbsolutePath());
 		init(manager, fullOntology, classes, prop, dataProp);
 
-		// Runnable doHelloWorld = new Runnable() { public void run() { //create
-		// a component for displaying and editing a graph.
-		// GraphComponent graphComponent = new GraphComponent(); // Enabling
-		// user-interaction
-		// graphComponent.setInputMode(new GraphEditorInputMode());
-		//
-		// IGraph graph = graphComponent.getGraph(); INode node1 =
-		// graph.createNode(new RectD(30, 30, 30, 30)); INode node2 =
-		// graph.createNode(new RectD(100, 30, 30, 30));
-		//
-		// IEdge edge1 = graph.createEdge(node1, node2);
-		//
-		// IPort portAtNode1 = graph.addPort(node1);
-		//
-		// ILabel ln1 = graph.addLabel(node1, "n1"); ILabel ln2 =
-		// graph.addLabel(node2, "n2");
-		//
-		// // create a top-level window and add the graph component. JFrame
-		// frame = new JFrame("Ontology Visualization");
-		//
-		// frame.setSize(500, 500); frame.setLocationRelativeTo(null);
-		// frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		// frame.add(graphComponent, BorderLayout.CENTER);
-		// frame.setVisible(true); } };
-		// SwingUtilities.invokeLater(doHelloWorld);
+		 Runnable doHelloWorld = new Runnable() { public void run() { /*create a component for displaying and editing a graph.*/
+		 GraphComponent graphComponent = new GraphComponent(); /*Enabling user-interaction*/
+		 graphComponent.setInputMode(new GraphEditorInputMode());
+		
+		 IGraph graph = graphComponent.getGraph(); 
+		 INode node1 = graph.createNode(new RectD(30, 30, 30, 30)); 
+		 INode node2 = graph.createNode(new RectD(100, 30, 30, 30));
+		
+		 IEdge edge1 = graph.createEdge(node1, node2);
+		
+		 IPort portAtNode1 = graph.addPort(node1);
+		
+		 ILabel ln1 = graph.addLabel(node1, "n1"); 
+		 ILabel ln2 = graph.addLabel(node2, "n2");
+		
+		 /*create a top-level window and add the graph component.*/ 
+		 JFrame frame = new JFrame("Ontology Visualization");
+		
+		 frame.setSize(500, 500); frame.setLocationRelativeTo(null);
+		 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		 frame.add(graphComponent, BorderLayout.CENTER);
+		 frame.setVisible(true); } };
+		 SwingUtilities.invokeLater(doHelloWorld);
 
 	}
 
