a0fa99670ebb341d22484158478d748458a7c4b7
Cogan Shimizu <shimizu.5@wright.edu>
2017-10-23 10:00:04 -0400

    removed unused code.

--- src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/Node.java
+++ src/main/java/dase/wright/edu/ontoViz/OntolologyVisualization/Node.java
@@ -1,43 +1,16 @@
 package dase.wright.edu.ontoViz.OntolologyVisualization;
 
-import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.EntityType;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.util.OWLEntityComparator;
-import org.semanticweb.owlapi.util.ShortFormProvider;
-import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
 public class Node
 {
-
-	String					entity;
-	EntityType<?>	type;
-	int						typeIndex;
+	private String			entity;
+	private EntityType<?>	type;
+	private int				typeIndex;
 
 	public int getTypeIndex()
 	{
 		return typeIndex;
-	}
-
-	public static ShortFormProvider		shortFormProvider	= new SimpleShortFormProvider();
-	public static OWLEntityComparator	entityComparator	= new OWLEntityComparator(shortFormProvider);
-
-	public String getShortForm()
-	{
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLDataFactory factory = manager.getOWLDataFactory();
-		IRI iri = IRI.create(entity);
-		OWLEntity cls = factory.getOWLEntity(EntityType.CLASS, iri);
-		String label = (escapeName(shortFormProvider.getShortForm(cls)));
-		return label;
-	}
-
-	public static String escapeName(String name)
-	{
-		return name.replace("_", "\\_").replace("#", "\\#");
 	}
 
 	public void setTypeIndex(int typeIndex)
